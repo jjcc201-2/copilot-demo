1e846891e01634deff30c95c15e32b01f571c00e
Josh Chalcraft <josh.chalcraft@outlook.com>
2023-03-29 16:43:41 +0100

    Basic functionality for express app

---
+++ express-app/README.md
@@ -0,0 +1,9 @@
+# Express-App
+
+This repository is to test Copilot features on a basic express application. Some of the features to try out include the following: 
+
+Code Completion:
+- Completing JavaScript objects based on the context of code 
+
+Documentation:
+- Listing the step by step process of a specific `app.get()` or `app.post()` function in plain English
--- express-app/index.js
+++ express-app/index.js
@@ -1,1 +1,27 @@
+const express = require('express');
 
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+var users = [
+
+]
+
+
+app.get('/users', (_req, res) => {
+    res.json(users)
+    console.log(`List of users requested by ${_req.ip}`)
+});
+
+app.post('/users', (req, res) => {
+    users.push(req.body)
+    res.status(200).send("success")
+    console.log(`Received new user from ${req.ip}, content: ${JSON.stringify(req.body)}`)
+});
+
+
+
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
